1ec379ebfa7c871382b133ccc723e86ceab6db76
satpro-hub <satpro@yandex.ru>
2024-03-14 20:00:02 +0300

    добавил middleware

--- src/JWTClient.php
+++ src/JWTClient.php
@@ -3,14 +3,73 @@
 namespace Satpro\JWTClient;
 class JWTClient
 {
-    public function __construct()
+    private static $expires_in = 3600;
+    private static $algorithm = OPENSSL_ALGO_SHA256;
+    private static $payload = null;
+    public static $validationErrors = [];
+
+    private static function jwtHeader()
     {
-        $this->process();
+        return ["alg" => "RS256", "typ" => "JWT"];
     }
 
-    private function process()
+    public static function getPayload()
     {
-        echo "test.....";
+        return self::$payload;
+    }
+
+    /**
+     * @param array $validationErrors
+     */
+    public static function addValidationErrors(array $validationErrors)
+    {
+        self::$validationErrors = array_merge(self::$validationErrors, $validationErrors);
+    }
+
+    private static function base64_url_decode($data)
+    {
+        return base64_decode(str_pad(strtr($data, '-_', '+/'), strlen($data) % 4, '=', STR_PAD_RIGHT));
+    }
+
+    private static function publicKey()
+    {
+        return file_get_contents(app('path.storage') . '/oauth-public.key');
+    }
+
+    /**
+     * Валидация токена, функция для middleWare
+     * @param $authorization_h
+     * @return bool
+     */
+    public static function validateToken($authorization_h): bool
+    {
+        $authorization = explode(" ", $authorization_h);
+        if (is_array($authorization) && $authorization[0] == "Bearer") {
+            $token = explode(".", $authorization[1] ?? null);
+
+            if (!is_array($token) || count($token) != 3) {
+                self::addValidationErrors(['invalid token format']);
+                return false;
+            }
+
+            self::$payload = json_decode(self::base64_url_decode($token[1]), true);
+
+            $ok = openssl_verify($token[0] . "." . $token[1], self::base64_url_decode($token[2] ?? null), self::publicKey(), self::$algorithm);
+            if ($ok == 1) {
+                if (self::$payload["exp"] < time()) {
+                    self::addValidationErrors(['token has expired']);
+                }
+                return true;
+            } elseif ($ok == 0) {
+                self::addValidationErrors(['invalid token']);
+            } else {
+                self::addValidationErrors(['error:' . openssl_error_string()]);
+            }
+        } else {
+            self::addValidationErrors(['Check Authorization']);
+        }
+
+        return false;
     }
 
 }
---
+++ src/JWTClientServiceProvider.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Satpro\JWTClient;
+
+use Illuminate\Support\ServiceProvider;
+
+class JWTClientServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        
+    }
+}
