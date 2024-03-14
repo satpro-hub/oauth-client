c7936a981f871dc80c0faf63f1ffedc45c03b0c9
satpro-hub <satpro@yandex.ru>
2024-03-14 08:42:30 +0300

    test commit

---
+++ src/JWTClient.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Satpro\JwtClient;
+class JWTClient
+{
+    public function __construct()
+    {
+        $this->process();
+    }
+
+    private function process()
+    {
+        echo "test.....";
+    }
+
+}
